d8d9baa5c0835124b2b21f9056e93ae92bd0ed5f
Muhambet <muhambet20111@gmail.com>
2020-04-16 17:35:18 +0600

    coomitt

---
+++ index.php
@@ -0,0 +1,105 @@
+<?php
+//echo "hello";
+
+/*$var = "hello";
+echo $var[0];
+echo $var[1];
+echo $var[4];
+*/
+
+/*$a = readline("Введите часы");
+echo $a*60;*/
+
+/*
+$text1=' Сәлем,';
+$text2='әлем!' ;
+echo $text1.$text2;
+*/
+
+/*
+$var = "ABCD"   ;
+$var[0] = "!";
+echo $var; 
+*/
+
+// $arr = ['Коля'=>'1000$', 'Вася'=>'500$', 'Петя'=>'200$'];
+// echo $arr["Коля"];
+// echo "\n";
+// echo $arr["Петя"];
+
+
+// $arr = [
+//     'ru'=>['голубой', 'красный', 'зеленый'],
+//     'en'=>['blue', 'red', 'green'],
+// ];
+// echo $arr['ru']['0'];
+
+
+// $arr = ['Коля'=>'a', 'Вася'=>'d', 'Петя'=>'e'];
+// //ksort($arr);
+// asort($arr); 
+// //sort($arr);
+// foreach($arr as $key =>$val)
+// {
+//     echo "$key => $val\\\n";
+// }
+
+
+// $a = 10; $b = 3;
+// echo $a%$b;
+
+// $a = 1;
+// if($a == 0)
+// {
+//     echo "Verno";
+// } 
+// else 
+// {
+//     echo "ne verno";
+// }
+
+// $a = 20;
+// $b = 1;
+// $c = 11;
+
+// if($a>$b && $a>$c){
+//     echo $a;
+//  }
+//  elseif ($b>$c && $b>$a){
+//      echo $b;
+//  }
+//  else{ echo $c;}
+
+
+// $arr = ['html', 'css', 'php', 'js', 'jq'];
+// foreach($arr as $key)
+// {
+//     echo "$key <br/>" ;
+// }
+
+// $arr = ['green'=>'зеленый', 'red'=>'красный' ,'blue'=>'голубой'];
+// foreach($arr as   $key =>$val)
+// {
+// echo "$key => $val \n";
+// }
+
+
+
+// $b = 0;
+// for($a=1;$a<101;$a++)
+// {     
+//     $b = $b + $a;    
+// }
+// echo $b;
+
+$sum = 0;
+$a = 1;
+while ($a <= 100) {
+    $sum += $a;
+    $a++;
+}
+
+echo "<br><br>Sum 100: ".$sum."<br>";
+
+// ?>
+
