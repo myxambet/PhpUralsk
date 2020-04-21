50c524eb0e2e98a03ab631d648f499073140e084
Muhambet <muhambet20111@gmail.com>
2020-04-21 23:12:33 +0600

    View in MVC

--- MyProj/Controllers/MainController.php
+++ MyProj/Controllers/MainController.php
@@ -1,17 +1,27 @@
 <?php
 namespace MyProj\Controllers;
-
+use MyProj\View\View;
 class MainController 
 {
-    public function main(){
-        echo "Главная страница";
-       
+    private $view;
+
+    public function __construct()
+    {
+        $this->view = new View(__DIR__ . '/../../templates');
     }
 
+    public function main()
+    {
+        $articles = [
+            ['name' => 'Статья 1', 'text' => 'Текст статьи 1'],
+            ['name' => 'Статья 2', 'text' => 'Текст статьи 2'],
+        ];
+        $this->view->renderHtml('main/main.php', ['articles' => $articles]);
+    }
 
     public function sayHello(string $name)
     {
-        echo "привет ".$name;
+        $this->view->renderHtml('main/hello.php', ['name' => $name]);
     }
 }
 
---
+++ MyProj/View/View.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MyProj\View;
+
+class View
+{
+    private $templatesPath;
+
+    public function __construct(string $templatesPath)
+    {
+        $this->templatesPath = $templatesPath;
+    }
+
+    public function renderHtml(string $templateName, array $vars = [])
+    {
+        extract($vars);
+
+        include $this->templatesPath . '/' . $templateName;
+    }
+}
